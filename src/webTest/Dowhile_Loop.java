7751d1a69cf44c1538a0f70b871fab6ea695e4f5
imran0422 <imran0422@gmail.com>
2022-01-27 20:29:24 -0600

    While_loop

---
+++ src/webTest/Dowhile_Loop.java
@@ -0,0 +1,13 @@
+package webTest;
+
+public class Dowhile_Loop {
+
+	public static void main(String[] args) {
+		int i = 0;
+		do {
+			System.out.println(i);
+		} while (true);
+
+	}
+
+}
--- src/webTest/Primenumber.java
+++ src/webTest/Primenumber.java
@@ -5,13 +5,13 @@ public class Primenumber {
 	public static void main(String[] args) {
 
 		for (int i = 1; i <= 100; i++) {
-			if (i % 5 == 0 ) {
+			if (i % 5 == 0) {
 				System.out.println("This is a Prime Number " + i);
-				if( i == 30) {
-					System.out.println("This is a Prime Number " + i);
+				if (i % 3 == 0) {
+					System.out.println("This is not a Prime Number" + i);
 				}
-			} else if (i%3 == 0) {
-				System.out.println("This is not a Prime Number" +i);
+			} else if (i == 30) {
+				System.out.println("This is a Prime Number " + i);
 			}
 		}
 
