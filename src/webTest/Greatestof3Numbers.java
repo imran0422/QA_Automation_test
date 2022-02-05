3af0be5618d424052fb7b6ee09fa97d2fadd6862
imran0422 <imran0422@gmail.com>
2022-02-04 20:33:52 -0600

    Greatestof3 Numbers Program

--- src/webTest/Greatestof3Numbers.java
+++ src/webTest/Greatestof3Numbers.java
@@ -1,17 +1,21 @@
 package webTest;
 
 public class Greatestof3Numbers {
-
+	static int A = 56;
+	static int B = 56;
+	static int C = 45;
+	
 	public static void main(String[] args) {
-		int A = 156;
-		int B = 56;
-		int C = 75;
+		String ab = "Error 2 or more Values are Equal";
 		if (A > B && A > C) {
 			System.out.println("A is Greater Than B & C ");
 		} else if (B > A && B > C) {
 			System.out.println("B is Greater Than A & C ");
 		} else if (C > A && C > B) {
 			System.out.println("C is Greater Than A & B ");
+		}
+		else {
+			System.out.println(ab);
 		}
 	}
 
