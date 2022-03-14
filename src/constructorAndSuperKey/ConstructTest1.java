3538c1708c6c9a5085662522859f50d0f6b5e25e
imran0422 <imran0422@gmail.com>
2022-03-14 17:22:33 -0500

    testNG

--- src/constructorAndSuperKey/ConstructTest1.java
+++ src/constructorAndSuperKey/ConstructTest1.java
@@ -1,21 +1,19 @@
 package constructorAndSuperKey;
 
 public class ConstructTest1 {
-	
+
 	public ConstructTest1() {
 		System.out.println("I am in the constructor");
 	}
-	public ConstructTest1(String name) {
-		System.out.println("My name is: "+name);
-	}
 
+	public ConstructTest1(String name) {
+		System.out.println("My name is: " + name);
+	}
 
 	public static void main(String[] args) {
 		ConstructTest1 obj = new ConstructTest1();
 		ConstructTest1 obj1 = new ConstructTest1("Imran");
-		
-	}
-	
 
+	}
 
 }
